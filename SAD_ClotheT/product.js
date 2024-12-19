9159c84ffb2bff58883eb9a2ee0bc14871cc8003
RitaChung1228 <147122827+RitaChung1228@users.noreply.github.com>
2024-12-20 05:48:40 +0800

    change 12/20

---
+++ SAD_ClotheT/product.js
@@ -0,0 +1,53 @@
+const product_json = [
+    {
+        "id": "Blue Soft Sweater",
+        "title": "Blue Soft Sweater",
+        "category": "Category/Sweater",
+        "image": "pictures/sweater1.png",
+        "condition": "90% New. In excellent condition, with minimal signs of wear.",
+        "size": "Medium (M)",
+        "material": "100% Wool",
+        "description": "Soft blue, perfect for the chilly season."
+    },
+    {
+        "id": "Cozy Knit Sweater",
+        "title": "Cozy Knit Sweater",
+        "category": "Category/Sweater",
+        "image": "pictures/sweater2.png",
+        "condition": "95% New",
+        "size": "Large (L)",
+        "material": "70% Cotton, 30% Wool",
+        "description": "Ideal for layering or wearing alone."
+    },
+    {
+        "id": "Elegant Sweater",
+        "title": "Elegant Sweater",
+        "category": "Category/Sweater",
+        "image": "pictures/sweater3.png",
+        "condition": "95% New",
+        "size": "Large (L)",
+        "material": "70% Cotton, 30% Wool",
+        "description": "Ideal for layering or wearing alone."
+    },
+    {
+        "id": "Classic Knit Sweater",
+        "title": "Classic Knit Sweater",
+        "category": "Category/Sweater",
+        "image": "pictures/sweater4.png",
+        "condition": "95% New",
+        "size": "Large (L)",
+        "material": "70% Cotton, 30% Wool",
+        "description": "Ideal for layering or wearing alone."
+    },
+    {
+        "id": "Fluffy Cardigan",
+        "title": "Fluffy Cardigan",
+        "category": "Category/Sweater",
+        "image": "pictures/sweater5.png",
+        "condition": "95% New",
+        "size": "Large (L)",
+        "material": "70% Cotton, 30% Wool",
+        "description": "Ideal for layering or wearing alone."
+    }
+
+]
